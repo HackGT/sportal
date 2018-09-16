2f83cabf8ffd4b235ce1fc4e06c21410dab80468
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-09-16 06:24:29 -0400

    Minor changes, WIP registration GraphQL Querying model

--- backend/controllers/user/renew.ts
+++ backend/controllers/user/renew.ts
@@ -3,7 +3,7 @@ import {Router} from "express";
 import {createToken} from "../../models/jwt/tokenModel";
 import ResponseCodes from "../../models/response/responseCodes";
 
-class RenewResponse {
+export class RenewResponse {
     public jwt: string;
 
     constructor(jwt: string) {
@@ -11,9 +11,9 @@ class RenewResponse {
     }
 }
 
-const router = Router();
+export const router = Router();
 
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
     const renewResponse = new RenewResponse(createToken(req.id as string,
         req.app.get("config").authSecret, req.app.get("config").authExp));
     res.status(ResponseCodes.SUCCESS);
