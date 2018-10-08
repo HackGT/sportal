cba995c42782407c854bbfa541f8e06191fe44e7
RSLi <robertsli@163.com>
2018-10-07 23:51:07 -0400

    Fix firefox NFC wss security error

--- client/src/constants/configs.js
+++ client/src/constants/configs.js
@@ -18,5 +18,6 @@ export const TITLE = 'HackGT Sponsorship Portal';
 export const IS_DEV_ENV = process.env.NODE_ENV === 'development';
 // API URL
 export const HOST = (!IS_DEV_ENV) ? (window.location.protocol + '//' + window.location.host + '/api') : 'http://localhost:8080/api';
-// NFC Websocket URL (see NFCService for more info)
-export const NFC_WS_URL = 'ws://localhost:1337/';
+// NFC Secure Websocket URL (see NFCService for more info)
+// Must be wss, since firefox does not allow ws with https
+export const NFC_WS_URL = 'wss://localhost:1337/';
